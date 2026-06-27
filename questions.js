b3e7334ae457588b5641c7d31f298a69a0135892
mathbootcamarhamnestacademy <mathbootcamp2025@gmail.com>
2026-06-27 14:01:43 +0530

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,52 @@
+const questions = [
+  {
+    question: "अहम् means",
+    options: ["I", "You", "He", "They"],
+    answer: 0
+  },
+  {
+    question: "त्वम् means",
+    options: ["You", "I", "He", "We"],
+    answer: 0
+  },
+  {
+    question: "बालकः means",
+    options: ["Girl", "Boy", "Teacher", "Friend"],
+    answer: 1
+  },
+  {
+    question: "फलम् means",
+    options: ["Flower", "Fruit", "Leaf", "Water"],
+    answer: 1
+  },
+  {
+    question: "गच्छति means",
+    options: ["Reads", "Goes", "Writes", "Runs"],
+    answer: 1
+  },
+  {
+    question: "पठति means",
+    options: ["Reads", "Eats", "Drinks", "Sleeps"],
+    answer: 0
+  },
+  {
+    question: "रामः is a",
+    options: ["Place", "Boy", "Animal", "Fruit"],
+    answer: 1
+  },
+  {
+    question: "सीता is a",
+    options: ["Girl", "Boy", "Tree", "Book"],
+    answer: 0
+  },
+  {
+    question: "वयम् means",
+    options: ["We", "You", "He", "She"],
+    answer: 0
+  },
+  {
+    question: "भवनम् means",
+    options: ["House", "River", "Tree", "School"],
+    answer: 0
+  }
+];
