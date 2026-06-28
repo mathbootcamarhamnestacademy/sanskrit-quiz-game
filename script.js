251df7f1b0b038aa4c3fd7a06706db5f11da967a
mathbootcamarhamnestacademy <mathbootcamp2025@gmail.com>
2026-06-28 09:53:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -289,3 +289,21 @@ function finishQuiz() {
         .catch(err => console.error("Database cloud syncing issue: ", err));
     }
 }
+// Make sure this is at the very top of your script.js file
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBEd1ylBG88RIF8EVvrxoS2tFFKhpPzGpQ",
+  authDomain: "sanskrit-quest.firebaseapp.com",
+  projectId: "sanskrit-quest",
+  storageBucket: "sanskrit-quest.firebasestorage.app",
+  messagingSenderId: "983562344506",
+  appId: "1:983562344506:web:66662e460343b070687d03"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
